141c6e4b26cdd7ea01339e961e46d87597aa3682
Christopher Yip <4493131+cyip92@users.noreply.github.com>
2024-04-04 20:14:46 +0100

    Exercise 3.4

--- index.ts
+++ index.ts
@@ -47,6 +47,12 @@ app.get("/api/persons/:id", (req: Request, res: Response) => {
   }
 });
 
+app.delete("/api/persons/:id", (req: Request, res: Response) => {
+  const id = Number(req.params.id);
+  persons = persons.filter(p => p.id !== id);
+  res.status(204).end();
+});
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
