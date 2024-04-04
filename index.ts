bc3f16be3178036ecf16056ca0dcc3a8e9ec9887
Christopher Yip <4493131+cyip92@users.noreply.github.com>
2024-04-04 20:34:13 +0100

    Exercise 3.6

--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@ import express from "express";
 import generateInfo from "./info";
 
 const app = express();
+app.use(express.json());
 
 let persons = [
   { 
@@ -34,6 +35,35 @@ app.get("/info", (req: Request, res: Response) => {
 
 app.get("/api/persons", (req: Request, res: Response) => {
   res.json(persons);
+});
+
+app.post("/api/persons", (req: Request, res: Response) => {
+  console.log(req.body)
+  const data = req.body;
+
+  let error = "";
+  if (!data?.name) {
+    error = "Cannot create entry with missing name";
+  } else if (!data?.number) {
+    error = "Cannot create entry with missing number";
+  } else if (persons.find(p => p.name === data.name)) {
+    error = `Person with name "${data.name}" already exists`;
+  } else if (persons.find(p => p.number === data.number)) {
+    error = `Number ${data.number} already exists`;
+  }
+
+  if (error === "") {
+    const newPerson = {
+      id: Math.floor(1e9 * Math.random()),
+      name: data.name,
+      number: data.number,
+    };
+  
+    persons = persons.concat(newPerson);
+    res.json(newPerson);
+  } else {
+    res.status(400).json({ error });
+  }
 });
 
 app.get("/api/persons/:id", (req: Request, res: Response) => {
