0a536508673e11adf6bc492f75421fe167cc56c4
Christopher Yip <4493131+cyip92@users.noreply.github.com>
2024-04-05 19:54:25 +0100

    Modify backend to use MongoDB

--- index.ts
+++ index.ts
@@ -1,13 +1,24 @@
+import "dotenv/config.js";
 import type { Request, Response } from "express";
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 
 import generateInfo from "./info";
+import Person from "./models/person";
 
+// This is used to display raw data for HTTP requests as per exercise 3.8
+// It"s bad practice to do this in general because of security reasons
 morgan.token("sensitiveData", function (req: Request) {
   return req.sensitiveData;
 });
+function getData(req: Request, res: Response, next:any) {
+  // The id is created on MongoDB now, so we want to remove it from the morgan logging
+  const filteredData = { ...req.body };
+  filteredData.id = undefined;
+  req.sensitiveData = JSON.stringify(filteredData);
+  next();
+}
 
 // Add all middleware
 const app = express();
@@ -17,80 +28,55 @@ app.use(morgan(":method :url :status :res[content-length] - :response-time ms :s
 app.use(getData);
 app.use(cors());
 
-let persons = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-];
-
 app.get("/info", (req: Request, res: Response) => {
-  res.send(generateInfo(persons));
+  Person.find({}).then(entries => {
+    res.send(generateInfo(entries));
+  });
 });
 
 app.get("/api/persons", (req: Request, res: Response) => {
-  res.json(persons);
+  Person.find({}).then(entries => {
+    res.json(entries);
+  });
 });
 
 app.post("/api/persons", (req: Request, res: Response) => {
   const data = req.body;
+  Person.find({}).then(entries => {
+    let error = "";
+    if (!data?.name) {
+      error = "Cannot create entry with missing name";
+    } else if (!data?.number) {
+      error = "Cannot create entry with missing number";
+    } else if (entries.find(p => p.name === data.name)) {
+      error = `Person with name "${data.name}" already exists`;
+    } else if (entries.find(p => p.number === data.number)) {
+      error = `Number ${data.number} already exists`;
+    }
 
-  let error = "";
-  if (!data?.name) {
-    error = "Cannot create entry with missing name";
-  } else if (!data?.number) {
-    error = "Cannot create entry with missing number";
-  } else if (persons.find(p => p.name === data.name)) {
-    error = `Person with name "${data.name}" already exists`;
-  } else if (persons.find(p => p.number === data.number)) {
-    error = `Number ${data.number} already exists`;
-  }
-
-  if (error === "") {
-    const newPerson = {
-      id: Math.floor(1e9 * Math.random()),
-      name: data.name,
-      number: data.number,
-    };
-  
-    persons = persons.concat(newPerson);
-    res.json(newPerson);
-  } else {
-    res.status(400).json({ error });
-  }
+    if (error === "") {
+      const newPerson = new Person({
+        name: data.name,
+        number: data.number,
+      });
+      newPerson
+        .save()
+        .then(savedNote => { res.json(savedNote) });
+    } else {
+      res.status(400).json({ error });
+    }
+  });
 });
 
 app.get("/api/persons/:id", (req: Request, res: Response) => {
-  const id = Number(req.params.id);
-  const foundPerson = persons.find(p => id === p.id);
-
-  if (foundPerson) {
-    res.json(foundPerson);
-  } else {
-    res.status(404).end();
-  }
+  Person.findById(req.params.id)
+    .then(entry => { res.json(entry); })
+    .catch(error => { res.status(404).end(); })
 });
 
 app.delete("/api/persons/:id", (req: Request, res: Response) => {
-  const id = Number(req.params.id);
-  persons = persons.filter(p => p.id !== id);
-  res.status(204).end();
+  Person.findByIdAndDelete(req.params.id)
+    .then(result => { res.status(204).end(); });
 });
 
 const unknownEndpoint = (req: Request, res: Response) => {
@@ -98,14 +84,7 @@ const unknownEndpoint = (req: Request, res: Response) => {
 };
 app.use(unknownEndpoint);
 
-// This is used to display raw data for HTTP requests as per exercise 3.8
-// It's bad practice to do this in general because of security reasons
-function getData(req: Request, res: Response, next:any) {
-  req.sensitiveData = JSON.stringify(req.body);
-  next();
-}
-
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 });
--- info.ts
+++ info.ts
@@ -5,7 +5,7 @@ type PhoneEntry = {
   number: string;
 };
 
-const generateInfo = (persons: Array<PhoneEntry>) => {
+const generateInfo = (persons: any) => {
   const count = `Phonebook has info for ${persons.length} ${persons.length === 1 ? "person" : "people"}.`;
   const time = String(new Date());
   return `${count}<br /><br />${time}`;
